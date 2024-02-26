6f48af73a3ea9dc8220a48a99be17d1f48fa9aeb
Reggie Hsu <reggiehsu111@gmail.com>
2024-02-26 12:54:38 +0800

    use vector

--- hnswlib/space_py.h
+++ hnswlib/space_py.h
@@ -3,15 +3,37 @@
 #include <functional>
 #include <iostream>
 #include <pybind11/pybind11.h>
+#include <vector>
 
 
 namespace py = pybind11;
 namespace hnswlib {
-py::function g_py_dist_func;
+static py::function g_py_dist_func;
 
 static float
-call_py_func(const void *a, const void *b, const void *c) {
-  return g_py_dist_func(a, b, c).cast<float>();
+call_py_func(const void *pVect1v, const void *pVect2v, const void *qty_ptr) {
+    float *pVect1 = (float *) pVect1v;
+    float *pVect2 = (float *) pVect2v;
+    int qty = *((int *) qty_ptr);
+    std::vector<float> v1;
+    std::vector<float> v2;
+    printf("qty: %d\n", qty);
+
+    float res = 0;
+    for (int i = 0; i < qty; i++) {
+        v1.push_back(*pVect1);
+        v2.push_back(*pVect2);
+        // printf("%d: %.6f, %.6f\n", i, *pVect1, *pVect2);
+        pVect1++;
+        pVect2++;
+    }
+    printf("Here\n");
+    if (g_py_dist_func) {
+      printf("Has py function\n");
+    }
+    float ret = g_py_dist_func(v1, v2).cast<float>();
+    printf("res: %.6f\n", res);
+    return ret;
 }
 
 class PySpace : public SpaceInterface<float> {
@@ -23,12 +45,11 @@ class PySpace : public SpaceInterface<float> {
  public:
     PySpace(const py::function &py_dist_func, size_t dim) {
         if (py_dist_func) {
-            int a=1, b=2, c=3;
+            float a=1, b=2;
+            int c=1;
             g_py_dist_func = py_dist_func;
             fstdistfunc_ = call_py_func;
             float res = fstdistfunc_(reinterpret_cast<const void *>(&a), reinterpret_cast<const void *>(&b), reinterpret_cast<const void *>(&c));
-            printf("%p, %p, %p\n", &a, &b, &c);
-            printf("res: %.6f\n", res);
         }
         // func_ = py_dist_func;
         printf("Here\n");
--- test.py
+++ test.py
@@ -1,33 +1,32 @@
 import hnswlib
 
-# Example Python code
-import ctypes
 
-def interpret_capsule_as_bytes(capsule):
-    # Check if the capsule is valid
-    if ctypes.pythonapi.PyCapsule_IsValid(capsule, b"byte_capsule"):
-        # Get the pointer from the capsule
-        ptr = ctypes.pythonapi.PyCapsule_GetPointer(capsule, b"byte_capsule")
-        if ptr:
-            # Get the size of the data (assuming it's null-terminated or has a known size)
-            size = 0
-            while ctypes.cast(ptr + size, ctypes.c_char).value != 0:
-                size += 1
-
-            # Create a bytes object from the data
-            data_bytes = ctypes.string_at(ptr, size)
-            return data_bytes
-
-    raise TypeError("Invalid byte_capsule")
+def create_100_data_points_around(num):
+    data = [[x] for x in range(num - 50, num + 50)]
+    labels = [x for x in range(num - 50, num + 50)]
+    return data, labels
 
 
-
-def dist(a, b, c):
+def dist(a, b):
     print("In dist")
-    print(a, b, c)
+    print(a, b)
     return 1.0
 
 
 dim = 1
-num_elements = 1000
+num_elements = 100
 p = hnswlib.Index(dist_func=dist, dim=1)
+
+p.init_index(max_elements=num_elements, ef_construction=20, M=10)
+
+p.add_items(*create_100_data_points_around(50))
+p.add_items(*create_100_data_points_around(250))
+p.add_items([[110], [190]], [110, 190])
+p.add_items([[120], [180]], [120, 180])
+p.add_items([[130], [170]], [130, 170])
+p.add_items([[140], [160]], [140, 160])
+
+labels, distances = p.knn_query([145], k=10)
+
+print(labels)
+print(distances)
